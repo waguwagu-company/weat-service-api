d5932e93778b1d6d95abfbb20669df9217b9df16
hyejinggu <118355536+hyejinggu@users.noreply.github.com>
2025-08-03 11:40:18 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/waguwagu/weat/WeatApplication.java
@@ -0,0 +1,13 @@
+package com.waguwagu.weat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waguwagu/weat/WeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waguwagu.weat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
