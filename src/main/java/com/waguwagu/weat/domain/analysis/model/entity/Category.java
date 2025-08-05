5c089232d37ed102cb40e63b66bcbb11be93c635
woody35545 <woody35545@gmail.com>
2025-08-06 00:36:14 +0900

    feat: #7 카테고리 엔티티 추가

---
+++ src/main/java/com/waguwagu/weat/domain/analysis/model/entity/Category.java
@@ -0,0 +1,26 @@
+package com.waguwagu.weat.domain.analysis.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "category")
+@Comment("카테고리")
+public class Category {
+
+    @Comment("카테고리 식별자")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id", columnDefinition = "INTEGER", nullable = false, updatable = false)
+    private Long categoryId;
+
+    @Comment("카테고리 명")
+    @Column(name = "category_name", columnDefinition = "VARCHAR(50)", length = 50)
+    private String categoryName;
+}
