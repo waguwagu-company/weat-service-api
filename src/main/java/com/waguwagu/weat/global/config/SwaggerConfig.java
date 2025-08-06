a89496acc4fa77b6ec0510f8669e1b03c10a1e48
woody35545 <woody35545@gmail.com>
2025-08-06 13:17:15 +0900

    chore: swagger server url 상대경로로 설정

--- src/main/java/com/waguwagu/weat/global/config/SwaggerConfig.java
+++ src/main/java/com/waguwagu/weat/global/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
@@ -21,7 +22,7 @@ public class SwaggerConfig {
                                 .name("waguwagu-company")
                                 .url("https://weat.kro.kr")
                         )
-                );
+                ).addServersItem(new Server().url("/"));
     }
 
     @Bean
