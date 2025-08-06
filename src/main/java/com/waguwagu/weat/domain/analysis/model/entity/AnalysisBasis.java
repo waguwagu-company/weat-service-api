0fb8e70403526816d38aed6c3ee362338f79c1c7
woody35545 <woody35545@gmail.com>
2025-08-07 03:39:41 +0900

    feat: #7 분석근거(AnalysisBasis) 엔티티 설정

---
+++ src/main/java/com/waguwagu/weat/domain/analysis/model/entity/AnalysisBasis.java
@@ -0,0 +1,28 @@
+package com.waguwagu.weat.domain.analysis.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "analysis_basis")
+public class AnalysisBasis {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "analysis_basis_id")
+    private Integer analysisBasisId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "analysis_result_detail_id", nullable = false)
+    private AnalysisResultDetail analysisResultDetail;
+
+    @Column(name = "analysis_basis_type", length = 50)
+    private String analysisBasisType;
+
+    @Column(name = "analysis_basis_content", columnDefinition = "text")
+    private String analysisBasisContent;
+}
