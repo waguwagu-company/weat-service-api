31a01342819b19604db5049988270c45d4b55374
woody35545 <woody35545@gmail.com>
2025-08-07 18:17:55 +0900

    docs: 멤버별 분석설정 제출 API 명세 추가

--- src/main/java/com/waguwagu/weat/domain/analysis/controller/AnalysisController.java
+++ src/main/java/com/waguwagu/weat/domain/analysis/controller/AnalysisController.java
@@ -5,6 +5,7 @@ import com.waguwagu.weat.domain.analysis.service.AnalysisService;
 import com.waguwagu.weat.domain.common.dto.ResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -25,7 +26,45 @@ public class AnalysisController {
             description = "성공",
             content = @Content(
                     mediaType = "application/json",
-                    schema = @Schema(implementation = SubmitAnalysisSettingDTO.Response.class)
+                    schema = @Schema(implementation = SubmitAnalysisSettingDtoResponseWrapper.class)
+            )
+    )
+    @ApiResponse(
+            responseCode = "208",
+            description = "이미 분석 설정을 제출한 멤버일 경우",
+            content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(
+                            name = "이미 제출됨",
+                            summary = "이미 제출한 회원입니다.",
+                            value = """
+                {
+                  "code": "MEMBER_ALREADY_SUBMIT_SETTING",
+                  "message": "이미 제출한 회원입니다.",
+                  "data": null
+                }
+                """
+                    )
+            )
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "요청한 리소스를 찾을 수 없을 경우",
+            content = @Content(
+                    mediaType = "application/json",
+                    examples = {
+                            @ExampleObject(
+                                    name = "해당하는 멤버 없음",
+                                    summary = "존재하지 않는 멤버 식별자",
+                                    value = """
+                                            {
+                                              "code": "MEMBER_NOT_FOUND",
+                                              "message": "존재하지 않는 멤버입니다. (memberId: ?)",
+                                              "data": null
+                                            }
+                                            """
+                            )
+                    }
             )
     )
     @PostMapping("/settings")
@@ -67,15 +106,14 @@ public class AnalysisController {
     // TODO: 개발 진행중, AI 분석 서비스 응답 형식 정해지면 재개
     @Operation(summary = "분석 시작 요청", description = "분석 시작 조건을 충족한 경우, 요청시점까지 제출된 분석설정을 활용하여 분석을 진행한다.")
     @PostMapping
-    public ResponseDTO<AnalysisStartDTO.Response> analysisStart(@RequestBody AnalysisStartDTO.Request request){
+    public ResponseDTO<AnalysisStartDTO.Response> analysisStart(@RequestBody AnalysisStartDTO.Request request) {
         return ResponseDTO.of(analysisService.analysisStart(request.getGroupId()));
     }
 
 
-
     @Operation(summary = "사용자 입력값 유효성 검증", description = "사용자가 입력한 값의 유효성을 검증한다.")
     @PostMapping("/validation/input")
-    public ResponseDTO<ValidationDTO.Response> validateInput(@RequestBody ValidationDTO.Request request){
+    public ResponseDTO<ValidationDTO.Response> validateInput(@RequestBody ValidationDTO.Request request) {
         return ResponseDTO.of(analysisService.validateInput(request));
     }
 }
