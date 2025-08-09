123bd695110b216c83c6809a24d08bd342c4aa48
Woody_K <woody35545@gmail.com>
2025-08-09 18:45:44 +0900

    fix: CORS 설정 오타 수정

--- src/main/java/com/waguwagu/weat/global/config/WebConfig.java
+++ src/main/java/com/waguwagu/weat/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://weat.kro.kr, http://localhost:3000")
+                .allowedOrigins("https://weat.kro.kr", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
