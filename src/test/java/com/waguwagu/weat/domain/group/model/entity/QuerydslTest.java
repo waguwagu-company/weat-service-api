24836f562fe817f8d193a4bc820c9fffc0c63eea
hyejinggu <118355536+hyejinggu@users.noreply.github.com>
2025-09-06 10:16:59 +0900

    chore: #78 테스트 코드에서 Lombok 어노테이션 인식되도록 의존성 추가

--- src/test/java/com/waguwagu/weat/domain/group/model/entity/QuerydslTest.java
+++ src/test/java/com/waguwagu/weat/domain/group/model/entity/QuerydslTest.java
@@ -2,6 +2,7 @@ package com.waguwagu.weat.domain.group.model.entity;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,13 +13,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
+@Slf4j
 public class QuerydslTest {
     @PersistenceContext
     EntityManager em;
 
     @Test
     void querydslTest() {
-        // TODO: 테스트 미완료 (DB 서버 세팅 후 .env 변경하여 테스트 완료하기)
         // given
         JPAQueryFactory queryFactory = new JPAQueryFactory(em);
         QGroup qGroup = QGroup.group;
@@ -32,6 +33,6 @@ public class QuerydslTest {
         assertThat(foundGroup).isNotNull();
         assertThat(foundGroup.getGroupId()).isNotBlank();
 
-        System.out.println("groupId = " + foundGroup.getGroupId());
+        log.info("groupId = {}", foundGroup.getGroupId());
     }
 }
