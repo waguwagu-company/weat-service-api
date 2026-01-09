87ec76753788ff6622b4bd1717049554d43f3bc6
gmk <woody35545@gmail.com>
2026-01-09 22:11:19 +0900

    feat: #100 카테고리 리스트 트리 구조 조회 기능 구현

--- src/main/java/com/waguwagu/weat/domain/category/repository/CategoryTagRepository.java
+++ src/main/java/com/waguwagu/weat/domain/category/repository/CategoryTagRepository.java
@@ -2,11 +2,14 @@ package com.waguwagu.weat.domain.category.repository;
 
 import com.waguwagu.weat.domain.category.model.entity.Category;
 import com.waguwagu.weat.domain.category.model.entity.CategoryTag;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CategoryTagRepository extends JpaRepository<CategoryTag, Long> {
-    List<CategoryTag> findAllByCategoryCategoryIdOrderByCategoryTagOrder(Long categoryId);
     List<CategoryTag> findByCategoryOrderByCategoryTagOrderDesc(Category category);
+
+    @EntityGraph(attributePaths = "category")
+    List<CategoryTag> findAllByCategory_CategoryIdInOrderByCategoryTagOrder(List<Long> categoryIds);
 }
--- src/main/java/com/waguwagu/weat/domain/category/service/CategoryService.java
+++ src/main/java/com/waguwagu/weat/domain/category/service/CategoryService.java
@@ -1,8 +1,10 @@
 package com.waguwagu.weat.domain.category.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.waguwagu.weat.domain.category.constant.CategoryTagStatus;
+import com.waguwagu.weat.domain.category.constant.CategoryVersion;
+import com.waguwagu.weat.domain.category.model.dto.CategoryDTO;
 import com.waguwagu.weat.domain.category.model.dto.GetAllCategoryListDTO;
-import com.waguwagu.weat.domain.category.model.entity.Category;
 import com.waguwagu.weat.domain.category.model.entity.CategoryTag;
 import com.waguwagu.weat.domain.category.repository.CategoryRepository;
 import com.waguwagu.weat.domain.category.repository.CategoryTagRepository;
@@ -12,7 +14,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -36,5 +43,101 @@ public class CategoryService {
                     .categoryList(new ArrayList<>())
                     .build();
         }
+    }
+
+
+    /**
+     * 카테고리 리스트를 트리구조로 조회
+     *
+     * @apiNote 카테고리 v1 버전이 더이상 사용되지 않음에 따라 기본적으로 v2 버전만 조회한다. (v1 버전의 카테고리는 추후 제거 예정)
+     */
+    public GetAllCategoryListDTO.Response getAllCategories() {
+
+        // 계층구조 없는 카테고리 리스트 조회
+        List<CategoryDTO> flatCategories = categoryRepository.fetchAllCategoryDtoByCategoryVersion(CategoryVersion.V2);
+
+        // 조회된 카테고리가 없는 경우 빈 리스트 반환
+        if (flatCategories.isEmpty()) {
+            return GetAllCategoryListDTO.Response.builder()
+                    .categoryList(List.of())
+                    .build();
+        }
+
+        // 조회된 카테고리 식별자에 해당하는 카테고리 태그 조회
+        List<CategoryTag> categoryTags = categoryTagRepository.findAllByCategory_CategoryIdInOrderByCategoryTagOrder(
+                flatCategories.stream().map(CategoryDTO::getCategoryId).toList()
+        );
+
+        // 카테고리 식별자별 태그 리스트 맵
+        Map<Long, List<GetAllCategoryListDTO.Response.CategoryTag>> tagsByCategoryIdMap = categoryTags.stream()
+                .collect(Collectors.groupingBy(
+                                ct -> ct.getCategory().getCategoryId(),
+                                Collectors.collectingAndThen(
+                                        Collectors.toList(),
+                                        list -> list.stream()
+                                                .sorted(Comparator
+                                                        .comparing(CategoryTag::getCategoryTagOrder)
+                                                        .thenComparing(CategoryTag::getCategoryTagId))
+                                                .map(t -> GetAllCategoryListDTO.Response.CategoryTag.builder()
+                                                        .categoryTagId(t.getCategoryTagId())
+                                                        .categoryTagOrder(t.getCategoryTagOrder())
+                                                        .label(t.getCategoryTagName())
+                                                        .status(CategoryTagStatus.DEFAULT)
+                                                        .build())
+                                                .toList()
+                                )
+                        )
+                );
+
+        // 카테고리 식별자별 응답 카테고리 맵
+        Map<Long, GetAllCategoryListDTO.Response.Category> resultCategoryMap = flatCategories.stream()
+                .map(dto ->
+                        GetAllCategoryListDTO.Response.Category.builder()
+                                .title(dto.getCategoryName())
+                                .categoryId(dto.getCategoryId())
+                                .categoryOrder(dto.getCategoryOrder())
+                                .tags(tagsByCategoryIdMap.getOrDefault(dto.getCategoryId(), List.of()))
+                                .children(new ArrayList<>())
+                                .build()
+                ).collect(Collectors.toMap(
+                                GetAllCategoryListDTO.Response.Category::getCategoryId,
+                                Function.identity()
+                        )
+                );
+
+        // 루트 카테고리 여부 판단 조건
+        Predicate<CategoryDTO> isRoot = dto -> dto.getCategoryParentId() == null;
+        Predicate<CategoryDTO> isNotRoot = isRoot.negate();
+
+        // 루트 카테고리인 경우 트리에 추가
+        List<GetAllCategoryListDTO.Response.Category> resultCategoryList = flatCategories.stream()
+                .filter(isRoot)
+                .map(dto -> resultCategoryMap.get(dto.getCategoryId()))
+                .toList();
+
+        // 루트 카테고리가 아닌 경우, 부모를 찾아 부모의 자식으로 추가
+        flatCategories.stream()
+                .filter(isNotRoot)
+                .forEach(dto -> {
+                            GetAllCategoryListDTO.Response.Category parent = resultCategoryMap.get(dto.getCategoryParentId());
+                            GetAllCategoryListDTO.Response.Category child = resultCategoryMap.get(dto.getCategoryId());
+
+                            if (parent == null) {
+                                log.warn("카테고리(childId={})의 부모 카테고리(parentId={}) 데이터가 존재하지 않습니다.", dto.getCategoryId(), dto.getCategoryParentId());
+                                return;
+                            }
+
+                            if (child == null) {
+                                log.warn("카테고리(childId={}) 데이터가 존재하지 않습니다.", dto.getCategoryId());
+                                return;
+                            }
+
+                            parent.getChildren().add(child);
+                        }
+                );
+
+        return GetAllCategoryListDTO.Response.builder()
+                .categoryList(resultCategoryList)
+                .build();
     }
 }
